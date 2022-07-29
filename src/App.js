825ca5ca5434dcc0d7c6034b2fc9956069041523
seobando <sebastianobandomorales@gmail.com>
2022-07-29 08:54:42 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { Component } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+constructor(){
+  super();
+
+  this.state = {
+    name: 'Sebas'
+  }
+}
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi {this.state.name}
+          </p>
+          <button onClick={()=>{
+            this.setState({name:'otherName'})
+          }}>
+            Change Name
+          </button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
