883810d6a7c128ec360c60605496831d982d6387
seobando <sebastianobandomorales@gmail.com>
2022-07-29 21:13:29 -0500

    class components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
 import { Component } from 'react';
-
-import logo from './logo.svg';
 import './App.css';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 
 class App extends Component {
-constructor(){
-  super();
+  constructor() {
+    super();
 
-  this.state = {
-    name: 'Sebas'
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
   }
-}
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(() => {
+        return { monsters: users };
+      },
+        () => {
+          console.log(this.state);
+        }
+      ))
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  }
 
   render() {
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name}
-          </p>
-          <button onClick={()=>{
-            this.setState({name:'otherName'})
-          }}>
-            Change Name
-          </button>
-        </header>
+        <h1 className='app-title'>Monster Rolodex</h1>
+        <SearchBox
+          className="search-box"
+          onChangeHandler={onSearchChange}
+          placeholder="search monsters"
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
